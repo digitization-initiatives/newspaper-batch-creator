64029d61b25aedb7973dc424af02ce97ee21f194
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-04-10 11:47:15 -0500

    Set up repo

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace NewspaperBatchAssemblyTool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
