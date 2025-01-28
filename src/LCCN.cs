41f2af004fd4ab47632634cd436347b87690cd45
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2025-01-28 22:46:50 +0000

    Add txa aggie

--- src/LCCN.cs
+++ src/LCCN.cs
@@ -13,7 +13,8 @@ namespace NewspaperBatchAssemblyTool.src
             //Texas A&M Newspapers
             { "sn37020267", new LCCN_PROPERTIES { TITLE = "The Daily Bulletin/Reveille.", BATCH_PREFIX = "batch_txa_daily_ver" } },
             { "sn86088544", new LCCN_PROPERTIES { TITLE = "The Battalion.", BATCH_PREFIX = "batch_txa_batt_ver" } },
-            { "sn88037063", new LCCN_PROPERTIES { TITLE = "The Texas Polka News.", BATCH_PREFIX = "batch_txa_polka_ver" } }
+            { "sn88037063", new LCCN_PROPERTIES { TITLE = "The Texas Polka News.", BATCH_PREFIX = "batch_txa_polka_ver" } },
+            { "sn84009780", new LCCN_PROPERTIES { TITLE = "The Texas Aggie.", BATCH_PREFIX = "batch_txa_aggie_ver" } }
         };
     }
 }
