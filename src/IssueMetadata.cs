ed518b59a754525072cd98b3ca1b1ac6faa34d62
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2025-10-30 22:36:09 +0000

    Refactor IssueMetadata class

--- src/IssueMetadata.cs
+++ src/IssueMetadata.cs
@@ -8,26 +8,33 @@ namespace NewspaperBatchCreator.src
 {
     public class IssueMetadata
     {
-        //Properties provided in column mapping DataGridView:
-        public string ISSUE_NUMBER { get; set; }
+        // Print issue metadata :
+        public string ITEM {  get; set; }
         public string TITLE { get; set; }
         public string DESCRIPTION { get; set; }
-        public string DATE { get; set; } 
+        public string DATE { get; set; }
         public string VOLUME { get; set; }
-        public string FREQUENCY { get; set; }
-        public string NUMBER_OF_PAGES { get; set; }
+        public string ISSUE { get; set; }
+        public string EDITION { get; set; }
+        public string PAGES { get; set; }
+
+        // Reel isue metadat :
         public string REEL_NUMBER { get; set; }
         public string REEL_SEQUENCE_NUMBER { get; set; }
-        public string DC_SUBJECT_INSTITUTION { get; set; }
-        public string DC_SUBJECT_COLLEGE {  get; set; }
-        public string DC_SUBJECT_LOCATION { get; set; }
-        public string DC_CONTRIBUTOR_COLLEGE { get; set; }
-        public string DC_CONTRIBUTOR_INSTITUTION { get; set; }
-        public string DC_COVERAGE { get; set; }
-        public string DC_PUBLISHER { get; set; }
-        public string DC_LANGUAGE { get; set; }
-        public string DC_FORMAT { get; set; }
-        public string DC_TYPE { get; set; }
-        public string DC_RIGHTS { get; set; }
+
+        public IssueMetadata()
+        {
+            ITEM = String.Empty;
+            TITLE = String.Empty;
+            DESCRIPTION = String.Empty;
+            DATE = String.Empty;
+            VOLUME = String.Empty;
+            ISSUE = String.Empty;
+            EDITION = String.Empty;
+            PAGES = String.Empty;
+
+            REEL_NUMBER = String.Empty;
+            REEL_SEQUENCE_NUMBER = String.Empty;
+        }
     }
 }
